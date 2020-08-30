89e4c0c52740946ced262cfa25dfe6276299fa8e
manikantajmv <68742392+manikantajmv@users.noreply.github.com>
2020-08-30 16:03:20 +0530

    Add files via upload

---
+++ eval5_28_08_2020/queries.sql
@@ -0,0 +1,27 @@
+use mydb;
+create table students(
+	rollno VARCHAR(50),
+    sname VARCHAR(50),
+    cgpa varchar(50),
+    company varchar(50)
+);
+
+
+insert into  students values ("1","tcs1","7","tcs");
+insert into  students values ("2","tcs2","7","tcs");
+insert into  students values ("3","tcs3","7","tcs");
+insert into  students values ("4","tcs4","7","tcs");
+insert into  students values ("5","tcs5","7","tcs");
+
+insert into  students values ("11","cts1","8","cts");
+insert into  students values ("12","cts2","8","cts");
+insert into  students values ("13","cts3","8","cts");
+insert into  students values ("14","cts4","8","cts");
+insert into  students values ("15","cts5","8","cts");
+
+insert into  students values ("21","inf1","6","infosys");
+insert into  students values ("22","inf2","6","infosys");
+insert into  students values ("23","inf3","6","infosys");
+insert into  students values ("24","inf4","6","infosys");
+insert into  students values ("25","inf5","6","infosys");
+
